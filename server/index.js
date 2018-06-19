e0220e2367c6e672dc4c41e92531f14d3259e239
Kyle McCarty <kylemccarty@Kyles-MacBook-Air.local>
2018-06-19 12:00:42 -0700

    add CORS in server

--- server/index.js
+++ server/index.js
@@ -2,9 +2,19 @@ const express = require('express')
 const helmet = require('helmet')
 const bodyParser = require('body-parser')
 const path = require('path')
+const cors = require('cors')
 
 const app = express()
 const PORT = 3333
+
+const corOpts = {
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+ }
+ 
+ app.use(cors(corOpts));
 
 app.use(express.static(path.join(__dirname, '../client/dist')))
 app.use(bodyParser.json())
